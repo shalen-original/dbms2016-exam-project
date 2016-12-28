3681a42a226afdda5337748d0b0934f75cbd68d0
Matteo Nardini <matteo.shalen@gmail.com>
2016-12-28 18:33:47 +0100

    Added file for common project constants

---
+++ GUI/Makerspace_Project_Management/src/mpm/main/ProjectConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Makerspace Project Management
+ * Database Systems 2016-2017
+ * Copyright 2016 (C) Grabocka Mikel, Nardini Matteo, Scolati Remo.
+ * All rights reserved.
+ */
+
+package mpm.main;
+
+/**
+ * Contains all the common project constants.
+ * Provides an easy way to avoid magic numbers and string in the program code
+ * @author Matteo Nardini
+ */
+public class ProjectConstants
+{
+    public static String DB_URL = "jdbc:postgresql://localhost:5432/dbs-16-17";
+    public static String DB_USERNAME = "postgres";
+    public static String DB_PASSWORD = "anothertmp";
+}
