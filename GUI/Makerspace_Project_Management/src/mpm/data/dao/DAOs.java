b9bcb08c1e75b0e5ce79b8de6e937faf467e1264
Matteo Nardini <matteo.shalen@gmail.com>
2017-01-03 22:03:45 +0100

    Added new class with static instances of all DAOs

---
+++ GUI/Makerspace_Project_Management/src/mpm/data/dao/DAOs.java
@@ -0,0 +1,20 @@
+/*
+ * Makerspace Project Management
+ * Database Systems 2016-2017
+ * Copyright 2016 (C) Grabocka Mikel, Nardini Matteo, Scolati Remo.
+ * All rights reserved.
+ */
+
+package mpm.data.dao;
+
+/**
+ * Contains a static instance of each DAO. If memory usage grows to much,
+ * this approach can be changed.
+ * @author Matteo Nardini
+ */
+public class DAOs {
+    
+    public static ProjectDAO project = new ProjectDAO();
+    public static FreeInfDAO free_inf = new FreeInfDAO();
+
+}
