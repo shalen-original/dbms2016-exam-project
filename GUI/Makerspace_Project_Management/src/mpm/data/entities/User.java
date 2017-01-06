e322f6f2ef25c47281eafdb88d887abe54ad39f0
Matteo Nardini <matteo.shalen@gmail.com>
2017-01-06 16:43:14 +0100

    lower-case on user email enforced

--- GUI/Makerspace_Project_Management/src/mpm/data/entities/User.java
+++ GUI/Makerspace_Project_Management/src/mpm/data/entities/User.java
@@ -50,7 +50,7 @@ public class User implements IUniquelyIdentifiable{
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email = email.toLowerCase();
     }
     
     @Override
