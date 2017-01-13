0015117d6ac2499ccb3c14fbcbb6d6b869c77f41
rscolati <remo_scolati@hotmail.com>
2017-01-13 13:19:24 +0100

    Changed user List in ProjectPanel to JTable

--- GUI/Makerspace_Project_Management/src/mpm/gui/ProjectPanel.java
+++ GUI/Makerspace_Project_Management/src/mpm/gui/ProjectPanel.java
@@ -15,6 +15,9 @@ import javafx.scene.layout.Border;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import mpm.data.dao.DAOs;
 import mpm.data.entities.Participation;
 import mpm.data.entities.ProjectRole;
@@ -92,7 +95,7 @@ public class ProjectPanel extends javax.swing.JPanel {
             projectTabbedPane.remove(addUserPanel);
             projectTabbedPane.remove(requestPanel);
         }     
-        
+               
     }
 
     /**
@@ -105,6 +108,7 @@ public class ProjectPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         userListPanel = new javax.swing.JPanel();
+        userScrollPane = new javax.swing.JScrollPane();
         projectTabbedPane = new javax.swing.JTabbedPane();
         overViewPanel = new javax.swing.JPanel();
         titleLabel = new javax.swing.JLabel();
@@ -112,7 +116,13 @@ public class ProjectPanel extends javax.swing.JPanel {
         descriptionArea = new javax.swing.JTextArea();
         descriptionLabel = new javax.swing.JLabel();
         userPanel = new javax.swing.JPanel();
-        userScrollPane = new javax.swing.JScrollPane();
+        userTableScrollPane = new javax.swing.JScrollPane();
+        userTable = new javax.swing.JTable();
+        selectedLabel = new javax.swing.JLabel();
+        selectedUserNameLabel = new javax.swing.JLabel();
+        changeRoleLabel = new javax.swing.JLabel();
+        selectedUserComboBox = new javax.swing.JComboBox<>();
+        deleteUserButton = new javax.swing.JButton();
         bookingPanel = new javax.swing.JPanel();
         requestPanel = new javax.swing.JPanel();
         addUserPanel = new javax.swing.JPanel();
@@ -121,9 +131,13 @@ public class ProjectPanel extends javax.swing.JPanel {
         backButton = new javax.swing.JButton();
 
         userListPanel.setMaximumSize(new java.awt.Dimension(51131, 45125));
-        userListPanel.setPreferredSize(null);
         userListPanel.setRequestFocusEnabled(false);
         userListPanel.setLayout(new javax.swing.BoxLayout(userListPanel, javax.swing.BoxLayout.PAGE_AXIS));
+
+        userScrollPane.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        userScrollPane.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        userScrollPane.setMaximumSize(new java.awt.Dimension(526, 3650));
+        userScrollPane.setPreferredSize(new java.awt.Dimension(526, 433));
 
         setMaximumSize(new java.awt.Dimension(640, 480));
         setMinimumSize(new java.awt.Dimension(640, 480));
@@ -180,20 +194,70 @@ public class ProjectPanel extends javax.swing.JPanel {
 
         projectTabbedPane.addTab("Overview", overViewPanel);
 
-        userScrollPane.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        userScrollPane.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        userScrollPane.setMaximumSize(new java.awt.Dimension(526, 3650));
-        userScrollPane.setPreferredSize(new java.awt.Dimension(526, 433));
+        userTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        userTableScrollPane.setViewportView(userTable);
+
+        selectedLabel.setText("Selected User:");
+
+        selectedUserNameLabel.setText("jLabel2");
+
+        changeRoleLabel.setText("Change role:");
+
+        selectedUserComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        selectedUserComboBox.setEnabled(false);
+
+        deleteUserButton.setText("remove user from project");
+        deleteUserButton.setEnabled(false);
 
         javax.swing.GroupLayout userPanelLayout = new javax.swing.GroupLayout(userPanel);
         userPanel.setLayout(userPanelLayout);
         userPanelLayout.setHorizontalGroup(
             userPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(userScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 554, Short.MAX_VALUE)
+            .addComponent(userTableScrollPane)
+            .addGroup(userPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(userPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(userPanelLayout.createSequentialGroup()
+                        .addComponent(selectedLabel)
+                        .addGap(18, 18, 18)
+                        .addComponent(selectedUserNameLabel)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(userPanelLayout.createSequentialGroup()
+                        .addComponent(changeRoleLabel)
+                        .addGap(31, 31, 31)
+                        .addComponent(selectedUserComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 84, Short.MAX_VALUE)
+                        .addComponent(deleteUserButton)
+                        .addGap(39, 39, 39))))
         );
         userPanelLayout.setVerticalGroup(
             userPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(userScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 393, Short.MAX_VALUE)
+            .addGroup(userPanelLayout.createSequentialGroup()
+                .addComponent(userTableScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(userPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(selectedLabel)
+                    .addComponent(selectedUserNameLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(userPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(userPanelLayout.createSequentialGroup()
+                        .addGroup(userPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(selectedUserComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(changeRoleLabel))
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, userPanelLayout.createSequentialGroup()
+                        .addComponent(deleteUserButton)
+                        .addGap(23, 23, 23))))
         );
 
         projectTabbedPane.addTab("Persons", userPanel);
@@ -214,7 +278,7 @@ public class ProjectPanel extends javax.swing.JPanel {
         settingsPanel.setLayout(settingsPanelLayout);
         settingsPanelLayout.setHorizontalGroup(
             settingsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 554, Short.MAX_VALUE)
+            .addGap(0, 540, Short.MAX_VALUE)
         );
         settingsPanelLayout.setVerticalGroup(
             settingsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -265,10 +329,26 @@ public class ProjectPanel extends javax.swing.JPanel {
         JTabbedPane e = (JTabbedPane)evt.getSource();
         
         if (e.getSelectedComponent().equals(userPanel))
-            reloadProjectUsers();
+            reloadUserTable();
         
     }//GEN-LAST:event_OnTabChange
 
+    private void listSelectionChanged(ListSelectionEvent e){
+    
+        int userID = (Integer) userTableModel.getValueAt(userTable
+                .getSelectedRow(), 0);
+        
+        selectedUserNameLabel.setText(DAOs.users.findByID(userID).getName());
+        
+        if(DAOs.participations.isUserAdminInProject(
+                MPM.currentUser.getId(), MPM.currentProject.getId())){
+            
+            selectedUserComboBox.setEnabled(true);
+            deleteUserButton.setEnabled(true);
+        }
+        
+    }
+    
     private void reloadProjectUsers()
     {
         userListPanel.removeAll();
@@ -286,11 +366,70 @@ public class ProjectPanel extends javax.swing.JPanel {
         userScrollPane.setViewportView (userListPanel);
     }
     
+    private void reloadUserTable(){
+        
+        List<Participation> participationList = DAOs.participations
+                .findByProjectID(MPM.currentProject.getId());
+        
+        userTable = new javax.swing.JTable();
+        userTable.setAutoCreateRowSorter(true);
+                
+        String[] projectColumnNames = {"Id",
+            "Name",
+            "Email",
+            "Project Role",
+            "Role"};
+        
+        userTableModel = new DefaultTableModel(
+                0, projectColumnNames.length);
+        
+        for(Participation p : participationList){
+            Object[] o = new Object[5];
+            o[0] = p.getUserId();
+            o[1] = DAOs.users.findByID(p.getUserId()).getName();
+            o[2] = DAOs.users.findByID(p.getUserId()).getEmail();
+            o[3] = p.getRole().toString();
+            // Ok, this one is really ugly, sorry. I got carried away.
+            o[4] = DAOs.roles.findByID(
+                    DAOs.users.findByID(p.getUserId())
+                            .getGeneralRoleId()).getName();
+                       
+            userTableModel.addRow(o);
 
+        }
+        
+        userTable.setModel(userTableModel);
+        userTableModel.setColumnIdentifiers(projectColumnNames);
+        userTableScrollPane.setViewportView(userTable);
+        userTable.setDefaultEditor(Object.class, null);
+        
+        selectedUserNameLabel.setText("<none>");
+        
+        if(DAOs.participations.isUserAdminInProject(
+                MPM.currentUser.getId(), MPM.currentProject.getId())){
+            
+            selectedUserComboBox.setEnabled(false);
+            deleteUserButton.setEnabled(false);
+        }else{
+        
+            userPanel.remove(selectedUserComboBox);
+            userPanel.remove(deleteUserButton);
+            userPanel.remove(changeRoleLabel);
+        }
+          
+        userTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                listSelectionChanged(e);}
+        });
+    }
+    
+    private DefaultTableModel userTableModel;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel addUserPanel;
     private javax.swing.JButton backButton;
     private javax.swing.JPanel bookingPanel;
+    private javax.swing.JLabel changeRoleLabel;
+    private javax.swing.JButton deleteUserButton;
     private javax.swing.JTextArea descriptionArea;
     private javax.swing.JLabel descriptionLabel;
     private javax.swing.JScrollPane jScrollPane2;
@@ -298,10 +437,15 @@ public class ProjectPanel extends javax.swing.JPanel {
     private javax.swing.JPanel overViewPanel;
     private javax.swing.JTabbedPane projectTabbedPane;
     private javax.swing.JPanel requestPanel;
+    private javax.swing.JLabel selectedLabel;
+    private javax.swing.JComboBox<String> selectedUserComboBox;
+    private javax.swing.JLabel selectedUserNameLabel;
     private javax.swing.JPanel settingsPanel;
     private javax.swing.JLabel titleLabel;
     private javax.swing.JPanel userListPanel;
     private javax.swing.JPanel userPanel;
     private javax.swing.JScrollPane userScrollPane;
+    private javax.swing.JTable userTable;
+    private javax.swing.JScrollPane userTableScrollPane;
     // End of variables declaration//GEN-END:variables
 }
